862af741087675860306a4a8c8e04b49b8adb3cf
wahyuseptiadi <wahyusptd@gmail.com>
2024-06-16 11:46:33 +0700

    rendering card list with dummy

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,27 +1,37 @@
 import CardProduct from "../components/Fragments/CardProduct"
 
+const productList = [
+    {
+        id: 1,
+        image: "./images/bycle-1.jpg",
+        title: "Bicycle v.1",
+        price: "Rp.10.999.000",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis adipisci nostrum recusandae veritatis voluptate eaque unde
+            sunt reiciendis quas accusamus tempore.`,
+    },
+    {
+        id: 2,
+        image: "./images/bycle-1.jpg",
+        title: "Bicycle v.2",
+        price: "Rp.12.400.000",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    },
+]
+
+
 const ProductPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header image="./images/bycle-1.jpg" />
-                <CardProduct.Body title="Bicycle v.1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Perferendis adipisci nostrum recusandae veritatis voluptate eaque unde
-                    sunt reiciendis quas accusamus tempore ratione ipsa natus aliquam
-                    voluptatem eveniet commodi, excepturi repudiandae.
-                </CardProduct.Body>
-                <CardProduct.Footer buttonText="Add to cart" price="Rp.10.999.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="./images/bycle-1.jpg" />
-                <CardProduct.Body title="Bicycle v.2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Perferendis adipisci nostrum recusandae veritatis voluptate
-                    eaque unde sunt reiciendis quas accusamus tempore.
-                </CardProduct.Body>
-                <CardProduct.Footer buttonText="Add to cart" price="Rp.12.400.000" />
-            </CardProduct>
+            {productList.map((product) => (
+                <CardProduct>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body title={product.title}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer buttonText="Add to cart" price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     )
 }
